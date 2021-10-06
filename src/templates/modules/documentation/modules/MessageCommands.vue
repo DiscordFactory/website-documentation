e41db6163d6659f5ce640b2eee5576101a72c84d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-10-06 18:09:34 +0200

    :pencil: Write documentation

---
+++ src/templates/modules/documentation/modules/MessageCommands.vue
@@ -0,0 +1,20 @@
+<template>
+  <Documentation>
+    <Markdown v-if="data" :source="data" />
+    <Spinner v-else />
+  </Documentation>
+</template>
+
+<script setup lang="ts">
+import Markdown from '../../../../components/Markdown.vue'
+import Documentation from '../../../../components/Documentation.vue'
+import Spinner from '../../../../components/Spinner.vue'
+import useDocumentation from '../../../../services/Documentation'
+import { markdownEndpoint } from '../../../../utils/Navigation'
+import { onMounted, ref } from 'vue'
+
+const data = ref('')
+onMounted(async () => {
+  data.value = await useDocumentation(markdownEndpoint.MESSAGE_COMMAND)
+})
+</script>
